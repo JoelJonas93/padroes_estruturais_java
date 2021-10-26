b273692ee354cf8c9cbeb114bf3bbab2b7cc3011
DESKTOP-TK0J234\Unity <joel.oliveira@fatec.sp.gov.br>
2021-10-26 16:03:00 -0300

    Implementation Composite

--- src/main/java/br/com/cod3r/composite/folders/Client.java
+++ src/main/java/br/com/cod3r/composite/folders/Client.java
@@ -1,8 +1,26 @@
 package br.com.cod3r.composite.folders;
 
-public class Client {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import br.com.cod3r.composite.folders.model.FileSystemItem;
+import br.com.cod3r.composite.folders.model.Folder;
+
+public class Client {
+	
 	public static void main(String[] args) {
+		FileSystemItem file1 = new br.com.cod3r.composite.folders.model.File("File1.txt");
+		FileSystemItem file2 = new br.com.cod3r.composite.folders.model.File("File2.txt");
+		FileSystemItem file3 = new br.com.cod3r.composite.folders.model.File("File3.txt");
+		FileSystemItem file4 = new br.com.cod3r.composite.folders.model.File("File4.txt");
+		FileSystemItem sf1 = new Folder("sf1", Arrays.asList(file1));
+		FileSystemItem sf2 = new Folder("sf2", Arrays.asList(file2, sf1));
+		FileSystemItem folder = new Folder("folder", Arrays.asList(sf2, file3, file4));
 		
+		folder.print("");
+		
+		System.out.println("------------");
 	}
 }
