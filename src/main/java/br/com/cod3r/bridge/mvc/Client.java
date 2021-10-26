7cb448903f763c462e061727630a2191aefdfb8c
DESKTOP-TK0J234\Unity <joel.oliveira@fatec.sp.gov.br>
2021-10-26 15:16:45 -0300

    Example Bridge with databases and other services

--- src/main/java/br/com/cod3r/bridge/mvc/Client.java
+++ src/main/java/br/com/cod3r/bridge/mvc/Client.java
@@ -1,13 +1,22 @@
 package br.com.cod3r.bridge.mvc;
 
+import br.com.cod3r.bridge.mvc.dao.UserOracleDao;
+import br.com.cod3r.bridge.mvc.dao.UserPostgresDao;
 import br.com.cod3r.bridge.mvc.model.User;
+import br.com.cod3r.bridge.mvc.services.UserRest;
+import br.com.cod3r.bridge.mvc.services.UserService;
+import br.com.cod3r.bridge.mvc.services.UserSoap;
 
 public class Client {
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		User user = new User("user", "user@email.com", "passwd");
 		
+		UserService userServiceOracle = new UserRest(new UserOracleDao());
+		userServiceOracle.save(user);
+
+		UserService userServicePostgres = new UserSoap(new UserPostgresDao());
+		userServicePostgres.save(user);
 		
 	}
 }
