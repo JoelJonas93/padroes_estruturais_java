b199232c4057c15579d3157c38931d107236dbb5
DESKTOP-TK0J234\Unity <joel.oliveira@fatec.sp.gov.br>
2021-10-26 14:51:19 -0300

    Implementation Bridge

--- src/main/java/br/com/cod3r/bridge/converter/Client.java
+++ src/main/java/br/com/cod3r/bridge/converter/Client.java
@@ -1,8 +1,25 @@
 package br.com.cod3r.bridge.converter;
 
+import br.com.cod3r.bridge.converter.converters.CSVConverter;
+import br.com.cod3r.bridge.converter.converters.Converter;
+import br.com.cod3r.bridge.converter.converters.JsonConverter;
+import br.com.cod3r.bridge.converter.employees.ITGuy;
+import br.com.cod3r.bridge.converter.employees.ProjectManager;
+
 public class Client {
 
 	public static void main(String[] args) {
+
+		Converter csvConverter = new CSVConverter();
+		Converter jsonConverter = new JsonConverter();
+
+		ITGuy itGuy = new ITGuy("Joel", 27, 18000.00);
+		ProjectManager projectManager = new ProjectManager("Augusto", 26, 7600.00);
+
+		System.out.println(csvConverter.getEmployeeFormated(itGuy));
+		System.out.println(jsonConverter.getEmployeeFormated(itGuy));
+		System.out.println(csvConverter.getEmployeeFormated(projectManager));
+		System.out.println(jsonConverter.getEmployeeFormated(projectManager));
 		
 	}
 }
